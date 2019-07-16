2b25d67af19af9f50c393d10d46c99a1bd05065d
AditiDesai <aditidesai0504@gmail.com>
2019-07-16 16:24:14 +0530

    AD: Added initial files.

---
+++ Weather/Weather/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Aditi Desai on 16/07/19.
+//  Copyright © 2019 Aditi Desai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
