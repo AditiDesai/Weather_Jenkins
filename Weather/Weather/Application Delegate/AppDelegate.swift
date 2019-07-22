99c3420f33a2905a6cd02851578e7117716da2fb
Aditi Desai <aditidesai0504@gmail.com>
2019-07-22 18:47:16 +0530

    AD: Trial2

--- Weather/Weather/Application Delegate/AppDelegate.swift
+++ Weather/Weather/Application Delegate/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Fabric.with([Crashlytics.self])
-        print("commit 1")
+        print("commit 2")
         return true
     }
 
